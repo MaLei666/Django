02e356b997a4fbd7e40c9bbf62e9bf5959fbc06c
malei <826908021@qq.com>
2018-11-15 21:58:43 +0800

    11-15

--- OPMS_v3/apps/spider_data/models.py
+++ OPMS_v3/apps/spider_data/models.py
@@ -5,11 +5,11 @@ class DataInfo(models.Model):
 
     question=models.CharField(verbose_name='问题标题', max_length=30)
     hot=models.CharField(verbose_name='问题热度', max_length=30)
-    answer_count=models.IntegerField(verbose_name='回答数', max_length=30)
-    text=models.CharField(verbose_name='回答内容', max_length=30)
+    answer_count=models.IntegerField(verbose_name='回答数')
+    text=models.CharField(verbose_name='回答内容', max_length=3000)
     author=models.CharField(verbose_name='回答作者', max_length=30)
-    voteup_count=models.IntegerField(verbose_name='赞同数量', max_length=30)
-    comment_count=models.IntegerField(verbose_name='评论数量', max_length=30)
+    voteup_count=models.IntegerField(verbose_name='赞同数量', )
+    comment_count=models.IntegerField(verbose_name='评论数量', )
     update_time=models.DateTimeField(verbose_name='更新时间', max_length=30)
 
     class Meta:
