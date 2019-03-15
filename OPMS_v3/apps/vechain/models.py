758660fe292e61acb4ee7c8f5d22c5996e8e705a
MaLei <826908021@qq.com>
2019-03-15 11:43:34 +0800

    03-15 更新

--- OPMS_v3/apps/vechain/models.py
+++ OPMS_v3/apps/vechain/models.py
@@ -5,11 +5,12 @@ from django.db import models
 ######################################
 class ArtifactInfo(models.Model):
     artifact_id = models.CharField(verbose_name='产品ID', max_length=255)
-    status = models.CharField(verbose_name='ID状态', max_length=20)
+    id_status = models.IntegerField(verbose_name='ID状态')
     add_time = models.DateTimeField(verbose_name='添加时间', auto_now_add=True)
-    update_user = models.CharField(verbose_name='修改人',max_length=100)
-    update_time = models.DateTimeField(verbose_name='修改时间', auto_now=True)
+    update_user = models.CharField(verbose_name='修改人',max_length=100, blank=True, null=True)
+    update_time = models.DateTimeField(verbose_name='修改时间', auto_now=True, blank=True, null=True)
     status = models.PositiveSmallIntegerField(verbose_name='状态', choices=((1, '正常'), (0, '停用')), default=1)
+
 
     class Meta:
         verbose_name = '产品信息'
